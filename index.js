28c88ee0beca3a595531c6ee2c0709a55995986c
Steven Fisher <stevenfisher.215@gmail.com>
2018-07-30 19:20:21 -0400

    Second Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+const config = require("./auth.json");
+
+client.config = config;
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("PING PING PING");
+  }
+});
+
+client.login(config.token);
