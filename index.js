38b9ca59fc45a356eb29cdf897319b9a34439a95
Steven Fisher <stevenfisher.215@gmail.com>
2018-07-30 21:24:18 -0400

    This bad boy sort of functions now.

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const Discord = require("discord.js");
+const path = require('path');
+const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./auth.json");
 
 client.config = config;
 
+const prefix = "$";
+
 client.on("ready", () => {
-  console.log("I am ready!");
+  console.log("Logged on and ready to distribute loot!!");
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("PING PING PING");
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if(!message.author.username.startsWith("ste")) {
+  	message.channel.send("NO COINS FOR YOU!");
+    return;
+  }
+
+  if (message.content.startsWith(prefix+"coins")) {
+    message.channel.send("Here! Have some coins!");
   }
 });
 
